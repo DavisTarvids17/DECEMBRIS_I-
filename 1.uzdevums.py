301fe83e3c9a9c7df49367762ebbd8003f24fcc4
DavisTarvids17 <davis.tarvids1@gmail.com>
2023-11-22 17:17:17 +0200

    2.uzdevums

--- 1.uzdevums.py
+++ 1.uzdevums.py
@@ -4,10 +4,10 @@ try:
     with open(faila_vards, 'r') as file:
         saturs = file.read()
 
-        print(f"Datnē ir {len(saturs)} simboli.")
-        print(f"Pirmie 10 simboli: {saturs[:10]}")
-        print(f"Pirmais simbols: {saturs[0]}, Pēdējais simbols: {saturs[-1]}")
-        garums = int(input("Lūdzu, ievadiet garumu:"))
+        print(f"Datnē ir {len(saturs)} simboli.") # Pirmais solis
+        print(f"Pirmie 10 simboli: {saturs[:10]}") # Otrais solis
+        print(f"Pirmais simbols: {saturs[0]}, Pēdējais simbols: {saturs[-1]}") # Trešais solis
+        garums = int(input("Lūdzu, ievadiet garumu:")) # Ceturtais solis
         print(f"Simboli no datnes sākuma līdz {garums}: {saturs[garums]}")
 
 except FileNotFoundError:
